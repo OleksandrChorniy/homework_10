d094d4fb54c44de5a906e2d15130e8bab5ceb2be
OleksandrChorniy <sahacherniy94@gmail.com>
2023-08-15 21:48:02 +0300

    commit

--- homework_10.py
+++ homework_10.py
@@ -1,7 +1,7 @@
 from collections import UserDict
 
 class Field:
-    def __init__(self, value):
+    def __init__(self, value=None):
         self.value = value
 
     def __str__(self):
@@ -14,36 +14,47 @@ class Phone(Field):
     pass
 
 class Record:
-    def __init__(self, name_value):
+    def __init__(self, name_value, *phones):
         self.name = Name(name_value)
-        self.phones = []
+        self.phones = [Phone(phone) for phone in phones]
 
-    def add_phone(self, phone_number):
-        phone = Phone(phone_number)
+    def add_phone(self, phone_value):
+        phone = Phone(phone_value)
         self.phones.append(phone)
 
-    def remove_phone(self, phone_number):
-        self.phones = [phone for phone in self.phones if str(phone) != phone_number]
+    def remove_phone(self, phone_value):
+        self.phones = [phone for phone in self.phones if phone.value != phone_value]
 
-    def edit_phone(self, old_phone_number, new_phone_number):
+    def edit_phone(self, old_value, new_value):
         for phone in self.phones:
-            if str(phone) == old_phone_number:
-                phone.value = new_phone_number
-                break
+            if phone.value == old_value:
+                phone.value = new_value
 
     def __str__(self):
-        phones_str = ", ".join(str(phone) for phone in self.phones)
-        return f"Name: {self.name}, Phones: {phones_str}"
+        phones_str = ', '.join(str(phone.value) for phone in self.phones)
+        return f"Name: {self.name.value}, Phones: {phones_str}"
 
 class AddressBook(UserDict):
     def add_record(self, record):
-        self.data[str(record.name)] = record
-
-    def find_records_by_name(self, name):
-        return [record for record in self.data.values() if str(record.name) == name]
-
-    def find_records_by_phone(self, phone):
-        return [record for record in self.data.values() if phone in [str(p) for p in record.phones]]
+        self.data[record.name.value] = record
 
     def __str__(self):
         return "\n".join(str(record) for record in self.data.values())
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+if __name__ == "__main__":
+    name = 'Bill'
+    phone = '1234567890'
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
+    for key, value in ab.items():
+        print(f"{key}: {value}")
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].phones, list)
+    assert isinstance(ab['Bill'].phones[0], Phone)
+    assert ab['Bill'].phones[0].value == '1234567890'
+    print('All Ok)')
